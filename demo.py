cce5baa84211aa3480d35579be2d4636ada65315
anautiyal998 <anautiyal998@gmail.com>
2020-01-23 22:09:12 +0530

    change 1

--- demo.py
+++ demo.py
@@ -1,12 +1,12 @@
 req=int(input("Enter the number of candies required by the user:"))
-avl=int(input("Enter the number of candies available in the vending machine:"))
-i=0
-while i<req:
-  if i>avl:
-      print("Stock Out!!!")
-      print("Have a nice Day!!!")
-      break
-  else:
-    print("Candy!!!")
-  i+=1
-print("Bye Pythonists..!!Nice meeting You!!!") 
+avl=int(input("Enter the number of candies available in the vending machine::"))
+i=1
+while i<=req:
+    if i>avl:
+        print("Stock Out!!!!")
+        print("Come again another day!!!")
+        break
+    print("Candy!!")
+    i+=1
+print("Bye Pythonists..!!!Nice meeting You!!!")
+print("Hello to GitHub!!!")
